00f0255cafd04d495845de840f9d94eb32a85c11
jim919 <dimitriosmariosg@gmail.com>
2023-02-14 12:13:11 +0200

    Update game-code.js

--- js/game-code.js
+++ js/game-code.js
@@ -517,23 +517,5 @@
 				textSize(50);
 				text('Game paused...', 500, 280);
 			}
+		}
 	
-	
-		function sendXHR(type, url, data, callback) {
-		var newXHR = new XMLHttpRequest() || new window.ActiveXObject("Microsoft.XMLHTTP");
-		newXHR.open(type, url, true);
-		newXHR.send(data);
-		newXHR.onreadystatechange = function() {
-			
-			if (this.status === 200 && this.readyState === 4) 
-			{
-				callback(this.response);
-			}
-		};
-	}
-	sendXHR("GET", ""C:\Users\Δημητρης Γιαν\OneDrive\Έγγραφα\GitHub\p5.asteroids\Pure-data\game.txt"", null, function(response) { // response contains the content of the description.txt file.
-	document.getElementById("description").innerHTML = response; // Use innerHTML to get or set the html content.
-});
-}
-
-
